b92b1bd4c259d075ccfd39b4db5603556ab8ea25
Anthony Hersey <anthony@stormsweeper.net>
2018-12-04 09:22:11 -0500

    Brute forcing day 2 part 2 I stil feel like there's a wy to sort the list approproiately, but I'm just throwing in the towel with the correct solution.

--- 2018/day02/similar.php
+++ 2018/day02/similar.php
@@ -4,17 +4,15 @@ $boxids = array_map(
     'trim',
     file($argv[1])
 );
-sort($boxids);
 
-
-for ($i = 1; $i < count($boxids); $i++) {
-    $a = $boxids[$i - 1];
-    $b = $boxids[$i];
-    if (levenshtein($a, $b) === 1) {
-        break;
+foreach ($boxids as $a) {
+    foreach ($boxids as $b) {
+        if (levenshtein($a, $b) === 1) {
+            break 2;
+        }
     }
 }
 
-echo "{$a}\n{$b}\n";
+echo "\n{$a}\n{$b}\n";
 
 echo implode('', array_intersect(str_split($a), str_split($b)));
