626fb78834809cf7b2946392acd318165ba96566
Anthony Hersey <anthony@stormsweeper.net>
2020-12-14 07:58:08 -0500

    2020 day 14 part 2 php

--- 2020/day14/php/docker.php
+++ 2020/day14/php/docker.php
@@ -33,4 +33,45 @@ foreach ($program as $inst) {
     $mem[$addr] = $val;
 }
 
-echo array_sum($mem);
+$p1 = array_sum($mem);
+echo "Part 1: {$p1}\n";
+
+$mask = str_repeat('0', 36);
+$mem = [];
+
+function addrs(string $mask, int $addr): array {
+    $addrs = [''];
+    $addr = str_pad(decbin($addr), 36, '0', STR_PAD_LEFT);
+    for ($i = 0; $i < 36; $i++) {
+        if ($mask[$i] === '0') {
+            $bits = [$addr[$i]];
+        } elseif ($mask[$i] === '1') {
+            $bits = [1];
+        } else {
+            $bits = [0,1];
+        }
+        $next = [];
+        foreach ($addrs as $a) {
+            foreach ($bits as $b) {
+                $next[] = $a.$b;
+            }
+        }
+        $addrs = $next;
+    }
+    return $addrs;
+}
+
+foreach ($program as $inst) {
+    if (strpos($inst, 'mask') === 0) {
+        $mask = substr($inst, 7);
+        continue;
+    }
+    preg_match('#\[(\d+)\] = (\d+)#', $inst, $m);
+    $val = intval($m[2]);
+    foreach(addrs($mask, $m[1]) as $addr) {
+        $mem[$addr] = $val;   
+    }
+}
+
+$p2 = array_sum($mem);
+echo "Part 2: {$p2}\n";
