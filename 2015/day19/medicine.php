d82f2e84b3c3a884112f7a8d31c43134646f9c1c
Anthony Hersey <anthony@stormsweeper.net>
2020-12-13 22:44:58 -0500

    2020 day 13 part 2 php shamelessly cribbed from @lizthegrey

--- 2015/day19/medicine.php
+++ 2015/day19/medicine.php
@@ -41,3 +41,34 @@ function next_steps(string $start, array $replacements): array {
 
 $p1 = count(next_steps($target, $replacements));
 echo "Part 1: {$p1}\n";
+
+function prior_steps(string $start, array $replacements): array {
+    $prior = [];
+    foreach ($replacements as $from => $tos) {
+        foreach ($tos as $to) {
+            $tlen = strlen($to);
+            foreach (strposes($start, $to) as $p) {
+                $r = substr_replace($start, $from, $p, $tlen);
+                if (strpos($r, 'e') !== false && $r !== 'e') continue;
+                $prior[$r] = 1;
+            }
+        }
+    }
+    return $prior;
+}
+
+$visited = $consider = [$target => $target];
+$its = 0;
+while (!isset($consider['e'])) {
+    $its++;
+    $next = [];
+    foreach ($consider as $mol => $_) {
+        foreach (prior_steps($mol, $replacements) as $prior => $_) {
+            if (isset($visited[$prior])) continue;
+            $next[$prior] = $visited[$prior] = 1;
+        }
+    }
+    $consider = $next;
+}
+
+echo "Part 2: {$its}\n";
--- 2020/day13/php/buses.php
+++ 2020/day13/php/buses.php
@@ -4,13 +4,15 @@ $inputs = trim(file_get_contents($argv[1]));
 list($etd, $routes) = explode("\n", $inputs);
 $routes = explode(',', $routes);
 
-$operating = array_filter($routes, function($r) {return $r !== 'x';});
+function closest(int $interval, int $min): int {
+    return ceil($min / $interval) * $interval;
+}
 
 $best_time = 2*$etd;
 $best_route = 0;
 foreach ($routes as $route) {
     if ($route === 'x') continue;
-    $dep = ceil($etd / $route) * $route;
+    $dep = closest($route, $etd);
     if ($dep < $best_time) {
         $best_time = $dep;
         $best_route = $route;
@@ -19,3 +21,17 @@ foreach ($routes as $route) {
 
 $score = ($best_time - $etd) * $best_route;
 echo "Part 1: {$score}\n";
+
+// shamelessly cribbed from @lizthegrey
+$operating = array_filter($routes, function($r) {return $r !== 'x';});
+$time = 0;
+$product = 1;
+
+foreach ($operating as $offset => $route) {
+    while (($time + $offset)%$route !== 0) {
+        $time += $product;
+    }
+    $product *= $route;
+}
+
+echo "Part 2: {$time}\n";
