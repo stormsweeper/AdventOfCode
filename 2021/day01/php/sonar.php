44d5d65aeb34a42a6e4b5df4dccb2af448506cba
Anthony Hersey <anthony@stormsweeper.net>
2021-12-01 09:08:43 -0500

    AoC 2021 day 1 part 2

--- 2021/day01/php/sonar.php
+++ 2021/day01/php/sonar.php
@@ -2,16 +2,37 @@
 
 $depths = fopen($argv[1], 'r');
 
-$last = 0;
-$changes = 0;
+$last_single = 0;
+$changes_single = 0;
+
+$idx = 0;
+$window = [];
+$last_triple = $changes_triple = 0; $last_changed = false;
 
 while (($depth = fgets($depths)) !== false) {
     $depth = intval($depth);
-    if ($last && $last < $depth) {
-        $changes++;
+    if ($last_single && $last_single < $depth) {
+        $changes_single++;
     }
-    $last = $depth;
+    $last_single = $depth;
+    // part 2
+    $window[$idx] = $depth;
+    if (count($window) === 3) {
+        $triple = array_sum($window);
+        if ($last_triple && $last_triple < $triple) {
+            $changes_triple++;
+            $last_changed = true;
+        } else {
+            $last_changed = false;
+        }
+        $last_triple = $triple;
+    }
+    $idx = ($idx + 1) % 3;
 }
 
-echo "changes: {$changes}\n";
+// drop if the last window wasn't comlete
+if ($idx < 2 && $last_changed) $changes_triple--;
+
+echo "single depth changes: {$changes_single}\n";
+echo "window depth changes: {$changes_triple}\n";
 
