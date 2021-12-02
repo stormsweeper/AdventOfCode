7fc0fcd849f4d4fa276a1814ed69b35145574575
Anthony Hersey <anthony@stormsweeper.net>
2021-12-02 11:08:35 -0500

    2021 day 2 part 2

--- 2021/day02/php/steering.php
+++ 2021/day02/php/steering.php
@@ -8,21 +8,31 @@ define('UP', 'up');
 
 $posY = $posX = 0;
 
+// p2
+$aim = 0;
+$depth = 0;
+
 while (($cmd = fgets($commands)) !== false) {
     list($dir, $val) = explode(' ', $cmd);
     $val = intval($val);
     if (!$val) continue;
     if ($dir === FORWARD) {
         $posX += $val;
+        $d = $depth + ($val * $aim);
+        $depth = max(0, $d);
     }
     elseif ($dir === UP) {
         $posY = max(0, $posY - $val);
+        $aim -= $val;
     }
     elseif ($dir === DOWN) {
         $posY += $val;
+        $aim += $val;
     }
-
 }
 
 $res = $posX * $posY;
 echo "part 1: {$res}\n";
+
+$res2 = $posX * $depth;
+echo "part 2: {$res2}\n";
