72b4b351b576e82edf62f10e1478d9ef7e327a7c
Anthony Hersey <anthony@stormsweeper.net>
2021-12-03 08:58:31 -0500

    refactor 2021 day 3 to prepare for part 2

--- 2021/day03/php/diagnostic.php
+++ 2021/day03/php/diagnostic.php
@@ -4,27 +4,47 @@ $inputs = trim(file_get_contents($argv[1]));
 $inputs = explode("\n", $inputs);
 $word_size = strlen($inputs[0]);
 
-$sums = [];
-foreach ($inputs as $word) {
-    for ($bit = 0; $bit < $word_size; $bit++) {
-        $sums[$bit] = ($sums[$bit] ?? 0) + $word[$bit];
-    }
+function int2word(int $d): string {
+    global $word_size;
+    return str_pad(decbin($d), $word_size, '0');
 }
 
-$half = count($inputs)/2;
-$gamma = $epsilon = '00000';
-
-foreach ($sums as $bit => $sum) {
-    if ($sum > $half) {
-        $gamma[$bit] = 1;
-        $epsilon[$bit] = 0;
-    }
-    else {
-        $gamma[$bit] = 0;
-        $epsilon[$bit] = 1;
-    }
+function word2int(string $w): int {
+    return intval(bindec($w));
 }
 
-$consumption = bindec($gamma) * bindec($epsilon);
+// using this vs ~ to flip leading 0s
+function invert_int(int $d): int {
+    global $word_size;
+    $max = word2int(str_repeat('1', $word_size));
+    return (~$d) & $max;
+}
 
-echo $consumption;
+function mc_bits(array $words): int {
+    global $word_size;
+    $sums = [];
+    foreach ($words as $word) {
+        for ($bit = 0; $bit < $word_size; $bit++) {
+            $sums[$bit] = ($sums[$bit] ?? 0) + $word[$bit];
+        }
+    }
+    
+    $half = count($words)/2;
+    $mc = '';
+    foreach ($sums as $bit => $sum) {
+        if ($sum >= $half) {
+            $mc[$bit] = 1;
+        }
+        else {
+            $mc[$bit] = 0;
+        }
+    }
+    return word2int($mc);
+}
+
+$gamma = mc_bits($inputs);
+$epsilon = invert_int($gamma);
+
+$consumption = $gamma * $epsilon;
+
+echo "gamma: {$gamma} epsilon: {$epsilon} consumption: {$consumption}\n";
