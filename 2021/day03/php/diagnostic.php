eca9b3961e178a47e13dec30b93a9cccb70bf2f9
Anthony Hersey <anthony@stormsweeper.net>
2021-12-03 09:34:11 -0500

    2021 day 3 part 2

--- 2021/day03/php/diagnostic.php
+++ 2021/day03/php/diagnostic.php
@@ -6,14 +6,14 @@ $word_size = strlen($inputs[0]);
 
 function int2word(int $d): string {
     global $word_size;
-    return str_pad(decbin($d), $word_size, '0');
+    return str_pad(decbin($d), $word_size, '0', STR_PAD_LEFT);
 }
 
 function word2int(string $w): int {
     return intval(bindec($w));
 }
 
-// using this vs ~ to flip leading 0s
+// using this vs just ~ to not flip leading 0s
 function invert_int(int $d): int {
     global $word_size;
     $max = word2int(str_repeat('1', $word_size));
@@ -39,6 +39,7 @@ function mc_bits(array $words): int {
             $mc[$bit] = 0;
         }
     }
+
     return word2int($mc);
 }
 
@@ -48,3 +49,29 @@ $epsilon = invert_int($gamma);
 $consumption = $gamma * $epsilon;
 
 echo "gamma: {$gamma} epsilon: {$epsilon} consumption: {$consumption}\n";
+
+// part 2
+
+$oxy = $co2 = $inputs;
+
+for ($bit = 0; $bit < $word_size; $bit++) {
+    if (count($oxy) > 1) {
+        $mc_o = int2word(mc_bits($oxy));
+        $oxy = array_filter($oxy, function($w) use ($bit, $mc_o) {
+            return $w[$bit] === $mc_o[$bit];
+        });
+    }
+
+    if (count($co2) > 1) {
+        $mc_c = int2word(mc_bits($co2));
+        $co2 = array_filter($co2, function($w) use ($bit, $mc_c) {
+            return $w[$bit] !== $mc_c[$bit];
+        });
+    }
+}
+
+$oxy = word2int(array_pop($oxy));
+$co2 = word2int(array_pop($co2));
+$support = $oxy * $co2;
+
+echo "oxy: {$oxy} co2: {$co2} support: {$support}\n";
