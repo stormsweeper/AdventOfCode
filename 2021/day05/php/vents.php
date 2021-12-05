29b28a71511222804f7798b298d16216455524b7
Anthony Hersey <anthony@stormsweeper.net>
2021-12-05 10:02:01 -0500

    2021 day 5 refactor to prep for part 2

--- 2021/day05/php/vents.php
+++ 2021/day05/php/vents.php
@@ -4,30 +4,33 @@ $inputs = trim(file_get_contents($argv[1]));
 $inputs = explode("\n", $inputs);
 
 // sparse array of ["x,y" => n]
-$grid = [];
+$hv_grid = $cart_grid = [];
 
 
 function addVent(&$grid, $x1, $y1, $x2, $y2) {
-    if ($x1 !== $x2) {
-        foreach (range($x1, $x2) as $x) {
-            $k = "{$x},{$y1}";
-            $grid[$k] = ($grid[$k]??0) + 1;
-        }    
+    $xrange = range($x1, $x2);
+    $yrange = range($y1, $y2);
+    $dist = max(count($xrange), count($yrange));
+    if (count($xrange) < $dist) {
+        $xrange = array_fill(0, $dist, $xrange[0]);
     }
-    elseif ($y1 !== $y2) {
-        foreach (range($y1, $y2) as $y) {
-            $k = "{$x1},{$y}";
-            $grid[$k] = ($grid[$k]??0) + 1;
-        }
+    if (count($yrange) < $dist) {
+        $yrange = array_fill(0, $dist, $yrange[0]);
+    }
+    for ($i = 0; $i < $dist; $i++) {
+        $k = "{$xrange[$i]},{$yrange[$i]}";
+        $grid[$k] = ($grid[$k]??0) + 1;
     }
 }
 
 foreach ($inputs as $line) {
     list($x1, $y1, $x2, $y2) = preg_split('/\D+/', $line);
     if ( $x1===$x2 || $y1===$y2 ) {
-        addVent($grid, $x1, $y1, $x2, $y2);
+        addVent($hv_grid, $x1, $y1, $x2, $y2);
     }
 }
 
-$danger = array_filter($grid, function($v) {return $v > 1;});
-echo count($danger);
+$hv_danger = array_filter($hv_grid, function($v) {return $v > 1;});
+$hv_danger = count($hv_danger);
+
+echo $hv_danger;
