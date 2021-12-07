e5681efa6e84542af42730641a8852c63cb4341d
Anthony Hersey <anthony@stormsweeper.net>
2021-12-07 10:55:00 -0500

    2021 day 7 part 2 (inefficient)

--- 2021/day07/php/crabs_in_subs.php
+++ 2021/day07/php/crabs_in_subs.php
@@ -6,17 +6,16 @@ $inputs = explode(',', $inputs);
 // map the count at each pos
 // [hpos => num]
 $crabs_at_hpos = [];
+$max_hpos = 0;
 foreach ($inputs as $hpos) {
+    $max_hpos = max($max_hpos, $hpos);
     $crabs_at_hpos[$hpos] = ($crabs_at_hpos[$hpos]??0) + 1;
 }
-
-// reverse sort the map by counts (e.g. in example 2 would be the first key)
-arsort($crabs_at_hpos);
 
 $best_cost = PHP_INT_MAX;
 $best_pos = -1;
 
-foreach ($crabs_at_hpos as $hpos_a => $_) {
+for ($hpos_a = 0; $hpos_a <= $max_hpos; $hpos_a++) {
     $cost = 0;
     foreach ($crabs_at_hpos as $hpos_b => $num) {
         $cost += abs($hpos_b - $hpos_a) * $num;
@@ -26,4 +25,26 @@ foreach ($crabs_at_hpos as $hpos_a => $_) {
     $best_pos = $hpos_a;
 }
 
-echo $best_cost;
+$p1_cost = $best_cost;
+
+// part 2
+
+$best_cost = PHP_INT_MAX;
+$best_pos = -1;
+
+for ($hpos_a = 0; $hpos_a <= $max_hpos; $hpos_a++) {
+    $cost = 0;
+    foreach ($crabs_at_hpos as $hpos_b => $num) {
+        $dist = abs($hpos_b - $hpos_a);
+        $dist_cost = 0;
+        for ($d = 0; $d <= $dist; $d++) $dist_cost += $d;
+        $cost += $dist_cost * $num;
+        if ($cost > $best_cost) continue 2;
+    }
+    $best_cost = $cost;
+    $best_pos = $hpos_a;
+}
+
+$p2_cost = $best_cost;
+
+echo "p1:{$p1_cost} p2:{$p2_cost}\n";
