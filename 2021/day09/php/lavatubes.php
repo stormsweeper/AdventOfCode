4f8ccf8778b813873cf272cf129f7e055b4e4bfd
Anthony Hersey <anthony@stormsweeper.net>
2021-12-09 11:12:03 -0500

    2021 day 9 part 2

--- 2021/day09/php/lavatubes.php
+++ 2021/day09/php/lavatubes.php
@@ -25,7 +25,7 @@ for ($y = 0; $y < INPUT_HEIGHT; $y++) {
         $k = pos2key($x,$y);
         $val = $inputs[$y][$x];
 
-        if ($val === 0) {
+        if ($val === '0') {
             $heightmap[$k] = LOW_POINT;
             // these will get overridden later if they need to be
             if ($x+1 < INPUT_WIDTH) $heightmap[pos2key($x+1, $y)] = BASIN_POINT;
@@ -33,7 +33,7 @@ for ($y = 0; $y < INPUT_HEIGHT; $y++) {
             continue;
         }
 
-        if ($val === 9) {
+        if ($val === '9') {
             $heightmap[$k] = HIGH_POINT;
             continue;
         }
@@ -65,13 +65,44 @@ for ($y = 0; $y < INPUT_HEIGHT; $y++) {
 
 $risk = 0;
 $basins = [];
+$checked = $consider = [];
+
+function adjkeys(string $key): array {
+    list($x,$y) = key2pos($key);
+    $adj = [];
+    if ($x-1 >= 0) $adj[] = pos2key($x-1,$y);
+    if ($x+1 < INPUT_WIDTH) $adj[] = pos2key($x+1,$y);
+    if ($y-1 >= 0) $adj[] = pos2key($x,$y-1);
+    if ($y+1 < INPUT_HEIGHT) $adj[] = pos2key($x,$y+1);
+    return $adj;
+}
+
 foreach ($heightmap as $k => $val) {
     if ($val === LOW_POINT) {
         list($x,$y) = key2pos($k);
         $risk += 1 + $inputs[$y][$x];
-
+        // p2
+        $basin_size = 1;
+        $checked = [$k=>$k];
+        $consider = adjkeys($k);
+        while ($consider) {
+            $next = [];
+            foreach ($consider as $adj) {
+                if (isset($checked[$adj])) continue;
+                $checked[$adj] = $adj;
+                if ($heightmap[$adj] === BASIN_POINT) {
+                    $basin_size++;
+                    $next = array_merge($next, adjkeys($adj));
+                }
+            }
+            $consider = array_diff($next, $checked);
+        }
+        $basins[$k] = $basin_size;
     }
 }
 
+rsort($basins);
 
-echo $risk;
+$top3 = $basins[0] * $basins[1] * $basins[2];
+
+echo "p1:{$risk} p2:{$top3}\n";
