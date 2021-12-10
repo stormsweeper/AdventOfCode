12619bfae4c09c4969f52e6c31b836927ac9e1ca
Anthony Hersey <anthony@stormsweeper.net>
2021-12-10 01:06:17 -0500

    2021 day 10 part 2

--- 2021/day10/php/syntax.php
+++ 2021/day10/php/syntax.php
@@ -5,6 +5,7 @@ $inputs = explode("\n", $inputs);
 
 define('VALID_CHUNKS', ['[]','{}','()','<>']);
 define('INVALID_SCORES', [')'=>3, ']'=>57, '}'=>1197, '>'=>25137]);
+define('INC_SCORES', ['('=>1, '['=>2, '{'=>3, '<'=>4]);
 
 function score_invalid(string $line): int {
     $replaced = 0;
@@ -19,5 +20,31 @@ function score_invalid(string $line): int {
     return 0;
 }
 
-$scores = array_map('score_invalid', $inputs);
-echo array_sum($scores);
+$invalid_score = 0;
+$inc_scores = [];
+foreach ($inputs as $line) {
+    // strip out the good chunks
+    $replaced = 0;
+    do {
+        $line = str_replace(VALID_CHUNKS, '', $line, $replaced);
+    }
+    while ($replaced > 0);
+    // find invalid
+    if (preg_match('/[\[(<{][\])>}]/', $line, $m)) {
+        $invalid_score += INVALID_SCORES[$m[0][1]];
+        continue;
+    }
+    // line now should only be incomplete part
+    $score = 0;
+    for ($i = strlen($line) - 1; $i >= 0; $i--) {
+        $score = $score * 5 + INC_SCORES[$line[$i]];
+    }
+    $inc_scores[] = $score;
+
+}
+
+sort($inc_scores);
+$med = $inc_scores[ floor(count($inc_scores)/2) ];
+
+echo "p1:{$invalid_score} p2:{$med}\n";
+
