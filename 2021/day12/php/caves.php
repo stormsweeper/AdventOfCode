06cd5df03b8e8f126ccfd088c95d0fdd1f7ca559
Anthony Hersey <anthony@stormsweeper.net>
2021-12-13 00:23:55 -0500

    simplified DFS impl

--- 2021/day12/php/caves.php
+++ 2021/day12/php/caves.php
@@ -7,22 +7,6 @@ function is_little(string $cave): bool {
     return strtolower($cave) === $cave;
 }
 
-function is_loopback(array $path): bool {
-    return $path[0] !== 'start';
-}
-
-function is_valid_exit(string $cave, array $path) {
-    if ($cave === 'start') return false;
-    if ($cave === 'end' || !is_little($cave)) return true;
-    if ($cave !== $path[0]) return !in_array($cave, $path);
-    // complicated version
-    $visits = 0;
-    foreach ($path as $c) {
-        if ($c === $cave) $visits++;
-    }
-    return $visits === 2; // i.e. the marker and the first visit
-}
-
 $cave_passages = [];
 foreach ($inputs as $line) {
     list($a, $b) = explode('-', $line);
@@ -32,44 +16,20 @@ foreach ($inputs as $line) {
     $cave_passages[$b][$a] = $a;
 }
 
-$consider = [
-    ['start'],
-];
-$rejected = $valid = $loopbacks = 0;
+function num_routes(string $cave, array $visited = []): int {
+    global $cave_passages;
 
-while ($consider) {
-    $next = [];
-    foreach ($consider as $wip_path) {
-        $last = $wip_path[array_key_last($wip_path)];
-        $passages = $cave_passages[$last];
-        if (!$passages) {
-            $rejected++;
-            continue;
-        }
-        foreach ($passages as $exit) {
-            if (!is_valid_exit($exit, $wip_path)) continue;
-            if ($exit === 'end') {
-                $npath = array_merge($wip_path, [$exit]);
-                if (is_loopback($npath)) {
-                    $loopbacks++;
-                }
-                else {
-                    $valid++;
-                }
-            }
-            else {
-                $npath = array_merge($wip_path, [$exit]);
-                $next[] = $npath;
-                if (!is_loopback($npath)) {
-                    array_unshift($npath, $exit);
-                    $next[] = $npath;
-                }
-            }
-        }
+    if ($cave === 'end') return 1;
+    if (isset($visited[$cave])) return 0;
+
+    if (is_little($cave)) $visited[$cave] = true;
+
+    $routes = 0;
+    foreach ($cave_passages[$cave]??[] as $exit) {
+        $routes += num_routes($exit, $visited);
     }
-    $consider = $next;
+    return $routes;
 }
 
-$p2 = $loopbacks + $p1;
+echo num_routes('start');
 
-echo "p1:{$valid} p2:{$p2}\n";
