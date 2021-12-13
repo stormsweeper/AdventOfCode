52fd58711eac244b0bfe1b683cea2922b9d31c80
Anthony Hersey <anthony@stormsweeper.net>
2021-12-13 11:14:02 -0500

    2021 day 13 part 2

--- 2021/day13/php/origami.php
+++ 2021/day13/php/origami.php
@@ -31,6 +31,8 @@ foreach ($inputs as $line) {
     list($axis, $amt) = explode('=', $line);
 
     $next = [];
+
+    $max_x = $max_y = 0;
     foreach ($dots as $pos => $_) {
         list($x, $y) = key2pos($pos);
         if ($axis === 'x') {
@@ -39,9 +41,19 @@ foreach ($inputs as $line) {
         else {
             $y = $amt - abs($y - $amt);
         }
+        $max_x = max($max_x, $x);
+        $max_y = max($max_y, $y);
         $next[pos2key($x, $y)] = 1;
     }
     $dots = $next;
     $num_folds++;
     echo "After fold {$num_folds}:" . array_sum($dots) . "\n";
+}
+
+
+for ($y = 0; $y <= $max_y; $y++) {
+    for ($x = 0; $x <= $max_x; $x++) {
+        echo empty($dots[pos2key($x, $y)]) ? ' ' : '#';
+    }
+    echo "\n";
 }
