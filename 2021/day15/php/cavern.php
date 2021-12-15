a1052a861a93beff3cbcd2f469de1f7ac3d866ec
Anthony Hersey <anthony@stormsweeper.net>
2021-12-15 13:28:12 -0500

    moving to Djikstra

--- 2021/day15/php/cavern.php
+++ 2021/day15/php/cavern.php
@@ -1,51 +1,80 @@
 <?php
 
-$cavern = trim(file_get_contents($argv[1]));
-$cavern = explode("\n", $cavern);
+$scan = trim(file_get_contents($argv[1]));
+$scan = explode("\n", $scan);
 
-define('CAVERN_WIDTH', strlen($cavern[0]));
-define('CAVERN_LENGTH', count($cavern));
+$grid_multi = intval($argv[2]??1);
 
-$cavern = implode('', $cavern);
+define('SCAN_WIDTH', strlen($scan[0]));
+define('SCAN_LENGTH', count($scan));
+define('CAVERN_WIDTH',  SCAN_WIDTH  * $grid_multi);
+define('CAVERN_LENGTH', SCAN_LENGTH * $grid_multi);
 
-function pos2i(int $x, int $y): string {
-    return ($y * CAVERN_WIDTH) + $x;
+$scan = implode('', $scan);
+
+function risklevel(string $pos): int {
+    global $scan;
+    [$x, $y] = key2pos($pos);
+    $sx = $x % SCAN_WIDTH;
+    $sy = $y % SCAN_LENGTH;
+    $r = $scan[scanpos2i($sx,$sy)];
+    $r += floor($x/SCAN_WIDTH);
+    $r += floor($y/SCAN_LENGTH);
+    return $r % 10;
 }
-function i2pos(int $i): array {
-    return [$i%CAVERN_WIDTH, floor($i/CAVERN_WIDTH)];
+
+function scanpos2i(int $x, int $y): string {
+    return ($y * SCAN_WIDTH) + $x;
 }
 
-function adj(int $i): array {
-    list($x, $y) = i2pos($i);
+function pos2key(int $x, int $y): string {
+    return "{$x},{$y}";
+}
+
+function key2pos(string $key): array {
+    [$x,$y] = explode(',', $key);
+    return [intval($x), intval($y)];
+}
+
+function adj(string $pos): array {
+    [$x, $y] = key2pos($pos);
     $adj = [];
     foreach ([[-1,0], [1,0], [0,-1], [0,1]] as [$dx, $dy])  {
         $ax = $x + $dx; $ay = $y + $dy;
         if ($ax < 0 || $ay < 0 || $ax >= CAVERN_WIDTH || $ay >= CAVERN_LENGTH) {
             continue;
         }
-        $adj[] = pos2i($ax, $ay);
+        $adj[] = pos2key($ax, $ay);
     }
     return $adj;
 }
 
+$start = pos2key(0, 0);
+$end   = pos2key(CAVERN_WIDTH - 1, CAVERN_LENGTH - 1);
+
 $shortest = [];
-
+$visited = [];
 $consider = [0];
+$queue = new SplPriorityQueue();
+$queue->insert([$start, 0], PHP_INT_MAX);
+$i = 0;
+while (!isset($visited[$end]) || $queue->valid()) {
+    [$node, $risk] = $queue->extract();
 
-while ($consider) {
+    // mark as visited
+    $visited[$node] = 1;
     $next = [];
-    foreach ($consider as $start) {
-        $sdist = $shortest[$start] ?? 0;
-        foreach (adj($start) as $a) {
-            $adist = $sdist + $cavern[$a];
-            if ($adist > ($shortest[$a]??PHP_INT_MAX)) continue;
-            $shortest[$a] = $adist;
-            $next[$a] = $a;
-        }
+    foreach (adj($node) as $adj) {
+        if (isset($visited[$adj])) continue;
+        $adjrisk = $risk + risklevel($adj);
+        $next[$adj] = min($adjrisk, $next[$adj]??PHP_INT_MAX);
     }
-    $consider = $next;
+    foreach ($next as $n => $r) {
+        $shortest[$n] = min($r, $shortest[$n]??PHP_INT_MAX);
+        $queue->insert([$n, $r], PHP_INT_MAX - $r);
+    }
 }
 
-$end = pos2i(CAVERN_WIDTH - 1, CAVERN_LENGTH - 1);
+// ksort($shortest);
+// print_r($shortest);
 echo $shortest[$end];
-
