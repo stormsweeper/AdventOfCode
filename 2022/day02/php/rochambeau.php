5ac1e54b7e2f5a4ce1bbcd0fb72d46d847cc95d1
Anthony Hersey <anthony@stormsweeper.net>
2022-12-02 13:07:02 -0500

    2022 day 2 php part 2

--- 2022/day02/php/rochambeau.php
+++ 2022/day02/php/rochambeau.php
@@ -16,6 +16,7 @@ $beats = [
     PAPER => SCISSORS,
     SCISSORS => ROCK,
 ];
+$loses = array_flip($beats);
 
 $values = [
     ROCK => 1,
@@ -23,7 +24,7 @@ $values = [
     SCISSORS => 3,
 ];
 
-function score_strat(string $strat): int {
+function score_strat_p1(string $strat): int {
     global $moves, $beats, $values;
     $opp_move = $moves[ $strat[0] ];
     $my_move = $moves[ $strat[2] ];
@@ -33,12 +34,32 @@ function score_strat(string $strat): int {
     return $values[$my_move];
 }
 
+function score_strat_p2(string $strat): int {
+    global $moves, $beats, $loses, $values;
+    $opp_move = $moves[ $strat[0] ];
+    $need = $strat[2];
+    // lose
+    if ($need === 'X') {
+        $my_move = $loses[ $opp_move ];
+        return $values[$my_move];
+    }
+    // draw
+    if ($need === 'Y') {
+        $my_move = $opp_move;
+        return $values[$my_move] + 3;
+    }
+    // win
+    $my_move = $beats[ $opp_move ];
+    return $values[$my_move] + 6;
+}
+
 // part 1
-$score = 0;
+$p1_score = $p2_score = 0;
 while (($strat = fgets($strats)) !== false) {
     $strat = trim($strat);
     if (!$strat) continue;
-    $score += score_strat($strat);
+    $p1_score += score_strat_p1($strat);
+    $p2_score += score_strat_p2($strat);
 }
 
-echo $score;
+echo "p1: {$p1_score} p2: {$p2_score} \n";
