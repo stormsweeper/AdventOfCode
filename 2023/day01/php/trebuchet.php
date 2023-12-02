4fe67d2ac7848f689c428df0d1d2000b002d6bd4
Anthony Hersey <anthony@stormsweeper.net>
2023-12-01 21:53:35 -0500

    Use strtr as a flex

--- 2023/day01/php/trebuchet.php
+++ 2023/day01/php/trebuchet.php
@@ -4,35 +4,33 @@ $calibrations = fopen($argv[1], 'r');
 
 $use_words = !empty($argv[2]);
 
-$words = [
-    'one' => 1,
-    'two' => 2,
-    'three' => 3,
-    'four' => 4,
-    'five' => 5,
-    'six' => 6,
-    'seven' => 7,
-    'eight' => 8,
-    'nine' => 9,
-];
 
 $total = 0;
 while (($cal = fgets($calibrations)) !== false) {
     if ($use_words) {
-        $digits = '';
-        for ($i = 0; $i < strlen($cal); $i++) {
-            if (is_numeric($cal[$i])) {
-                $digits .= $cal[$i];
-                continue;
-            }
-            // words can overlap like 'eighttwo'
-            foreach ($words as $w => $d) {
-                if (strpos($cal, $w, $i) === $i) {
-                    $digits .= $d;
-                    continue 2;
-                } 
-            }
-        }
+        $cal = strtr(
+            $cal,
+            [
+                'one' => 1,
+                'two' => 2,
+                'three' => 3,
+                'four' => 4,
+                'five' => 5,
+                'six' => 6,
+                'seven' => 7,
+                'eight' => 8,
+                'nine' => 9,
+                // these evil ones are throughtout the input
+                'oneight' => 18,
+                'twone' => 21,
+                'threeight' => 38,
+                'fiveight' => 58,
+                'sevenine' => 79,
+                'eightwo' => 82,
+                'eighthree' => 83,
+                'nineight' => 98,
+            ]
+        );
     }
     $cal = trim($cal, "abcdefghijklmnopqrstuvwxyz\n");
     $line = 0;
