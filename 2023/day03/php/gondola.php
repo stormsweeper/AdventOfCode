2427073b9b4044f7bfc3aa678594709c504e9f19
Anthony Hersey <anthony@stormsweeper.net>
2023-12-03 10:53:36 -0500

    2023 day 3 php gondola

---
+++ 2023/day03/php/gondola.php
@@ -0,0 +1,89 @@
+<?php
+
+$schematic = explode("\n", trim(file_get_contents($argv[1])));
+// these are equal in inputs, but not relying on that
+$width = strlen($schematic[0]);
+$height = count($schematic);
+$max_x = $width - 1;
+$max_y = $height - 1;
+
+$sum = 0;
+$gear_sum = 0;
+
+function read_schematic(int $x, int $y): string {
+    global $schematic;
+    return $schematic[$y][$x] ?? '.';
+}
+
+function is_symbol(int $x, int $y): bool {
+    global $schematic;
+    $spot = read_schematic($x, $y);
+    return $spot !== '.' && !is_numeric($spot);
+}
+
+function is_part_digit(int $x, int $y): bool {
+    global $schematic;
+    return 
+        is_symbol($x - 1, $y) || is_symbol($x + 1, $y) || is_symbol($x, $y - 1) || is_symbol($x, $y + 1) || 
+        is_symbol($x - 1, $y - 1) || is_symbol($x + 1, $y + 1) || is_symbol($x - 1, $y + 1) || is_symbol($x + 1, $y - 1)
+    ;
+}
+
+function number_at(int $x, int $y): int {
+    global $width;
+    $num = '';
+    $spot = read_schematic($x, $y);
+    if (is_numeric($spot)) {
+        $num = $spot;
+        for ($lx = $x - 1; $lx > -1; $lx--) {
+            $left = read_schematic($lx, $y);
+            if (!is_numeric($left)) break;
+            $num = $left . $num;
+        }
+        for ($rx = $x + 1; $rx < $width; $rx++) {
+            $right = read_schematic($rx, $y);
+            if (!is_numeric($right)) break;
+            $num = $num . $right;
+        }
+    }
+    return intval($num);
+}
+
+function gear_ratio(int $x, int $y): int {
+    $spot = read_schematic($x, $y);
+    if ($spot !== '*') return 0;
+
+    $gears = [];
+    foreach ([-1, 0, 1] as $dx) {
+        foreach ([-1, 0, 1] as $dy) {
+            if ($dx === 0 && $dy === 0) continue;
+            $gear = number_at($x + $dx, $y + $dy);
+            if ($gear) $gears[] = $gear;
+        }
+    }
+    $gears = array_unique($gears);
+    if (count($gears) !== 2) return 0;
+    return array_product($gears);
+}
+
+for ($y = 0; $y < $height; $y++) {
+    $current_number = 0;
+    $is_part_num = false;
+    for ($x = 0; $x < $width; $x++) {
+        $spot = read_schematic($x, $y);
+        if (is_numeric($spot)) {
+            $current_number = $current_number*10 + $spot;
+            $is_part_num = $is_part_num || is_part_digit($x, $y);
+        } else {
+            if ($current_number > 0) {
+                if ($is_part_num) $sum += $current_number;
+                $current_number = 0;
+                $is_part_num = false;
+            }
+            $gear_sum += gear_ratio($x, $y);
+        }
+    }
+    if ($is_part_num) $sum += $current_number;
+}
+
+echo "p1: {$sum}\np2: {$gear_sum}\n";
