25a9c211f64e55d388b54af7e67fbaba8517ee87
Anthony Hersey <anthony@stormsweeper.net>
2023-12-04 08:57:24 -0500

    2023 day 4 part 2 php

--- 2023/day04/php/scratchcards.php
+++ 2023/day04/php/scratchcards.php
@@ -3,19 +3,37 @@
 $cards = fopen($argv[1], 'r');
 
 
-function score_card(string $card): int {
+function count_matching(string $card): int {
     [, $numbers] = explode(': ', $card, 2);
     [$winning, $mine] = explode(' | ', $numbers, 2);
     $winning = array_filter(explode(' ', $winning));
     $mine = array_filter(explode(' ', $mine));
     $matching = array_intersect($mine, $winning);
-    if (!$matching) return 0;
-    return pow(2, count($matching) - 1);
+    return count($matching);
 }
 
 $total = 0;
+$scored = [];
 while (($card = fgets($cards)) !== false) {
-    $total += score_card(trim($card));
+    $scored[] = $matching = count_matching(trim($card));
+    if ($matching) $total += pow(2, $matching - 1);
 }
 
-echo $total . "\n";
+echo "part 1: {$total}\n";
+
+$unique_cards = array_keys($scored);
+function count_all_cards(array $cards, int $depth = 0): int {
+    global $unique_cards, $scored;
+    $count = count($cards);
+    foreach ($cards as $c) {
+        if ($scored[$c]) {
+            $extra = array_slice($unique_cards, $c + 1, $scored[$c]);
+            $count += count_all_cards($extra, $depth + 1);
+        }
+    } 
+    return $count;
+}
+
+$total_cards = count_all_cards($unique_cards);
+
+echo "part 2: {$total_cards}\n";
