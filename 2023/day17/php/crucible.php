7db905aa1337227c835879ed503bd7b7c8fd21e7
Anthony Hersey <anthony@stormsweeper.net>
2024-01-04 18:14:09 -0500

    non-working djikstra for day 17

--- 2023/day17/php/crucible.php
+++ 2023/day17/php/crucible.php
@@ -12,7 +12,7 @@ function is_oob(int $x, int $y): bool {
     return $x < 0 || $x > $max_x || $y < 0 || $y > $max_y;
 }
 
-function heat_loss_at(int $x, int $y): int {
+function heat_loss_from(int $x, int $y): int {
     global $city;
     if (is_oob($x, $y)) return -1;
     return intval($city[$y][$x]);
@@ -25,19 +25,23 @@ class Crucible {
         return new self($x, $y, $dir, $heat_loss, $pushes);
     }
 
+    // reverse sort 
     static function sift(Crucible $a, Crucible $b): int {
-        return $b->weight() <=> $a->weight();
+        return $b->dist() <=> $a->dist();
     }
 
     function __construct(
         public int $x, public int $y, public string $dir, public int $heat_loss, public int $pushes
     ) {}
 
+    function poskey(): string { return "{$this->x},{$this->y};{$this->dir}"; }
     function hashkey(): string { return "{$this->x},{$this->y};{$this->dir};{$this->pushes}"; }
+
+    // manhattan distance from end
+    function dist(): int { return $this->x + $this->y; }
 
     function weight(): int {
         global $max_x, $max_y;
-        // manhattan distance from end
         $heuristic = 1 * ($max_x - $this->x + $max_y - $this->y);
         return $this->heat_loss + $heuristic;
     }
@@ -58,7 +62,7 @@ class Crucible {
         if ($this->dir === 'L') $new_x--;
         if ($this->dir === 'R') $new_x++;
         if (is_oob($new_x, $new_y)) return null;
-        $new_heat_loss = $this->heat_loss + heat_loss_at($new_x, $new_y);
+        $new_heat_loss = $this->heat_loss + heat_loss_from($new_x, $new_y);
         return self::create( $new_x, $new_y, $this->dir, $new_heat_loss, $this->pushes + 1); 
     }
     function left():  ?Crucible {
@@ -69,7 +73,7 @@ class Crucible {
         if ($this->dir === 'L') { $new_y++; $new_dir = 'D'; }
         if ($this->dir === 'R') { $new_y--; $new_dir = 'U'; }
         if (is_oob($new_x, $new_y)) return null;
-        $new_heat_loss = $this->heat_loss + heat_loss_at($new_x, $new_y);
+        $new_heat_loss = $this->heat_loss + heat_loss_from($new_x, $new_y);
         return self::create( $new_x, $new_y, $new_dir, $new_heat_loss, 1); 
     }
     function right(): ?Crucible {
@@ -80,42 +84,47 @@ class Crucible {
         if ($this->dir === 'L') { $new_y--; $new_dir = 'U'; }
         if ($this->dir === 'R') { $new_y++; $new_dir = 'D'; }
         if (is_oob($new_x, $new_y)) return null;
-        $new_heat_loss = $this->heat_loss + heat_loss_at($new_x, $new_y);
+        $new_heat_loss = $this->heat_loss + heat_loss_from($new_x, $new_y);
         return self::create( $new_x, $new_y, $new_dir, $new_heat_loss, 1); 
     }
 
     function next(): array { return array_filter([$this->forward(),$this->left(),$this->right()]); }
 }
 
-$consider = $visited = [];
+$consider = $visited = $min_heat_loss = [];
 
 $start_down = Crucible::create(0, 0, 'D', 0, 0);
 $consider[$start_down->hashkey()] = $start_down;
+$min_heat_loss[$start_down->poskey()] = 0;
+
 $start_right = Crucible::create(0, 0, 'R', 0, 0);
 $consider[$start_right->hashkey()] = $start_right;
+$min_heat_loss[$start_right->poskey()] = 0;
 
-$min_heat_loss = PHP_INT_MAX;
+$end = null;
 do {
     uasort($consider, 'Crucible::sift');
 
     $crucible = array_pop($consider);
     // echo $crucible->hashkey() . "\n";
     if ($crucible->isAtEnd()) {
-        $min_heat_loss = min($min_heat_loss, $crucible->heat_loss);
+        $end = $crucible;
         break;
     }
 
     foreach ($crucible->next() as $next) {
         if (isset($visited[$next->hashkey()])) continue;
 
-        $prior = $consider[$next->hashkey()] ?? null;
-        if (!isset($prior) || $next->heat_loss < $prior->heat_loss) {
+        $prior_heat = $min_heat_loss[$next->poskey()] ?? PHP_INT_MAX;
+        if ($next->heat_loss < $prior_heat) {
+            $min_heat_loss[$next->poskey()] = $next->heat_loss;
+
             $consider[$next->hashkey()] = $next;
         }
     }
 
-    $visited[$crucible->hashkey()] = $crucible;
+    $visited[$crucible->hashkey()] = 1;
 
 } while (!empty($consider));
 
-echo "{$min_heat_loss}\n";
+echo "{$end->heat_loss}\n";
