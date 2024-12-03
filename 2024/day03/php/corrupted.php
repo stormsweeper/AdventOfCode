0d09070dd77866623a5830a07f3af6ab828db21a
Anthony Hersey <anthony@stormsweeper.net>
2024-12-03 18:21:06 -0500

    2024 day 3 part 2 php

--- 2024/day03/php/corrupted.php
+++ 2024/day03/php/corrupted.php
@@ -12,3 +12,26 @@ foreach ($m[1] as $i => $a) {
 }
 
 echo "p1: {$score}\n";
+
+// well, p2 is also just a regex, but using a diff match order
+$cmd_regex = '#mul\((\d+),(\d+)\)|do\(\)|don\'t\(\)#';
+preg_match_all($cmd_regex, $input, $cmds, PREG_SET_ORDER);
+
+$enabled = true;
+$score2 = 0;
+foreach ($cmds as $cmd) {
+    if ($cmd[0] === 'do()') {
+        $enabled = true;
+        continue;
+    }
+    if ($cmd[0] === 'don\'t()') {
+        $enabled = false;
+        continue;
+    }
+
+    if (!$enabled) continue;
+
+    $score2 += $cmd[1] * $cmd[2];
+}
+
+echo "p2: {$score2}\n";
